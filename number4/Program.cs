9c7f17486cff8efe0ce92e879e22ce9d41cef48c
Ademola Balogun <abalogun@softalliance.co.ng>
2023-12-20 15:19:04 +0100

    first commit

---
+++ number4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+
+    public static void Main()
+    {
+        var numberList = new List<int>();
+
+        int rawNumber;
+        bool check;
+
+        do
+        {
+            var rawEntry = Console.ReadLine();
+            check = int.TryParse(rawEntry, out rawNumber);
+
+            if (check == false && rawEntry.ToLowerInvariant() == "quit")
+            {
+                Console.WriteLine("List contains duplicate values but presented below are only unique numbers:");
+
+                // using Linq
+                List<int> distinctList = numberList.Distinct().ToList();
+                foreach (var element in distinctList)
+                    Console.WriteLine(element);
+
+                break;
+
+            }
+            if (check == false)
+                Console.WriteLine("To exit type quit or enter more numbers");
+
+            else
+                numberList.Add(rawNumber);
+
+        }
+        while (true);
+
+    }
+}
